ae88624273e9361086982d3f202f0316897a0277
kgpksh <tkdgns543@gmail.com>
2024-04-20 18:53:05 +0900

    feat: start project

---
+++ src/test/java/com/codecrafter/spring_with_flutter/SpringWithFlutterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecrafter.spring_with_flutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithFlutterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
